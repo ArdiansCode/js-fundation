5b8a49857b723b9cc58615ef2317ee76042c0e89
ArdiansCode <ardiansyah6246@gmail.com>
2026-02-05 08:56:54 +0700

    add javascript functions practice

--- day-2-logic.js
+++ day-2-logic.js
@@ -2,6 +2,16 @@
 /*
 tampilkan nama produk yang  memiliki harga lebih dari 10000 dan stok lebih dari 0
 */
+function getAvailableExpensiveProducts(products) {
+    let result = [];
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].price > 10000 && products[i].stock > 0) {
+            result.push(products[i].name)
+        }
+    }
+    return result;
+} 
+
 const product = [
     { name: "Pensil", price: 2000, stock: 10 },
     { name: "Buku", price: 12000, stock: 5 },
@@ -10,45 +20,52 @@ const product = [
     { name: "Tas", price: 75000, stock: 3 }
 ]
 
-for (let i = 0; i < product.length; i++) {
-    if (product[i].price > 10000 && product[i].stock > 0) {
-        console.log(product[i]. name);
-    }
-}
+console.log(getAvailableExpensiveProducts(product));
+
 
 
 // tugas 2
 /*
 cari angka ganjil di array tersebut, lalu jumlahkan angka ganjil tersebut
 */
-const numbers = [1, 4, 7, 10, 13, 18];
-const result = [];
-let end_result = 0;
+function sumOddNumbers(numbers) {
+    const result = [];
+    let end_result = 0;
 
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 == 1) {
-        result.push(numbers[i]);
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] % 2 == 1) {
+            result.push(numbers[i]);
+        }
     }
+
+    for (let r = 0; r < result.length; r++) {
+        end_result += result[r];
+    }
+
+    return end_result;
 }
 
-for (let r = 0; r < result.length; r++) {
-    end_result += result[r];
-    console.log(end_result);
-}
+const number = [1, 4, 7, 10, 13, 18];
+console.log(sumOddNumbers(number));
 
 
 // tugas 3
 /*
 tampilkan nama user yang berumur lebih dari 20 tahun
 */
-const users = [
-  { name: "Andi", age: 17 },
-  { name: "Budi", age: 21 },
-  { name: "Citra", age: 25 }
+function getAdultUsers(users) {
+    let result = [];
+    for (let u = 0; u < users.length; u++) {
+        if (users[u].age > 20)  {
+            result.push(users[u].name);
+        }
+    }
+    return result;
+}
+const user = [
+    { name: "Andi", age: 17 },
+    { name: "Budi", age: 21 },
+    { name: "Citra", age: 25 }
 ];
 
-for (let u = 0; u < users.length; u++) {
-    if (users[u].age > 20)  {
-        console.log(users[u].name);
-    }
-}
+console.log(getAdultUsers(user));
